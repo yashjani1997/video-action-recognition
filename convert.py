66a89ecbd4f1d73a12e0ba19e8889628df0e22df
Yash Jani <yashjani445@gmail.com>
2026-03-07 13:15:43 +0530

    version issue

--- convert.py
+++ convert.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
-model = tf.keras.models.load_model("models/action_recognition_final.keras", compile=False)
+print("TF version:", tf.__version__)
+
+model = tf.keras.models.load_model(
+    "models/action_recognition_final.keras", 
+    compile=False
+)
 model.save("models/action_recognition_final.h5")
 print("Done!")
